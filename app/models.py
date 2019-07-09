86053e01ed1d284b32ac6490cdfcf8aabb205748
Antony <amwaniki180@gmail.com>
2019-07-09 15:41:55 +0300

    created a post page that enables the user to post

--- app/models.py
+++ app/models.py
@@ -3,3 +3,11 @@ from flask_login import UserMixin
 from app import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 
+class Post(db.Model):
+    __tablename__ = "posts"
+    id  = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post = db.Column(db.String)
+    time = db.Column(db.String)
+    comments = db.relationship("Comment",backref = "post", lazy = "dynamic")
