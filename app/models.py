5c5ed77f9ab60a2b4be897ba464ae8e9037b354b
Francis Mukuha <mukuha58@gmail.com>
2019-07-09 16:03:50 +0300

    add class user and class comment

--- app/models.py
+++ app/models.py
@@ -1,23 +1,51 @@
 from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from . import login_manager
-from datetime import datetime
-from  flask_migrate import Migrate, MigrateCommand
+from app import login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    """
+    Class  to create users
+    """
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key = True)
+    full_name = db.Column(db.String)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    bio = db.Column(db.String)
+    image = db.Column(db.String)
+    post = db.relationship("Post", backref = "user", lazy = "dynamic")
+    user_pass = db.Column(db.String)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @property
+    def password(self):
+        raise AttributeError("Gerrarahia")
+
+    @password.setter
+    def password(self,password):
+        self.user_pass = generate_password_hash(password)
+
+    def verify_pass(self,password):
+        return check_password_hash(self.user_pass, password)
+
 
 class Comment(db.Model):
-    __tablename__='comments'
-
-    id = db.Column(db.Integer,primary_key=True)
-    comment_content = db.Column(db.String())
-    pitch_id = db.Column(db.Integer)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    time = db.Column(db.String)
 
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
-
-    @classmethod
-    def get_comments(cls,id):
-        comments = Comment.query.filter_by(pitch_id=id).all()
-        return comments
