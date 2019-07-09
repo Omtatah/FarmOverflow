510db00305cdd609c0a1b3fc7472e5661cc3007a
Francis Mukuha <mukuha58@gmail.com>
2019-07-09 17:15:17 +0300

    edit classes

--- app/models.py
+++ app/models.py
@@ -2,26 +2,33 @@ from . import db
 from flask_login import UserMixin
 from app import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+import os
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(UserMixin,db.Model):
-    __tablename__ = 'users'
-
+    """
+    Class  to create users
+    """
+    __tablename__ = "users"
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(40),unique = True, index=True)
-    hash_pass = db.Column(db.String(255))
-    email = db.Column(db.String(255),unique = True, index = True)
-    profile_pic_path = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, index = True)
     bio = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    user_pass = db.Column(db.String(255))
 
-    pitches = db.relationship('Pitch',backref='user',lazy='dynamic')
+    posts = db.relationship('Post',backref='user',lazy='dynamic')
     comments = db.relationship('Comment',backref='user',lazy='dynamic')
     upvotes = db.relationship('UpVote',backref='user',lazy='dynamic')
     downvotes = db.relationship('DownVote',backref='user',lazy='dynamic')
     photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
 
     @property
     def password(self):
@@ -29,79 +36,60 @@ class User(UserMixin,db.Model):
 
     @password.setter
     def password(self,password):
-        self.hash_pass = generate_password_hash(password)
+        self.user_pass = generate_password_hash(password)
 
-    def set_password(self,password):
-        self.hash_pass = generate_password_hash(password)
-
-    def verify_password(self,password):
-        return check_password_hash(self.hash_pass,password)
-
-    def get_reset_password_token(self, expires_in=600):
-        return jwt.encode({'reset_password':self.id, 'exp':time()+expires_in}, os.environ.get('SECRET_KEY'), algorithm='HS256').decode('utf-8')
-
-    @staticmethod
-    def verify_reset_password(token):
-        try:
-            id = jwt.decode(token, os.environ.get('SECRET_KEY'),algorithms=['HS256'])['reset_password']
-        except:
-            return
-        return User.query.get(id)
+    def verify_pass(self,password):
+        return check_password_hash(self.user_pass, password)
 
     def __repr__(self):
         return f'User {self.username}'
 
-class Pitch(db.Model):
-    __tablename__ = 'pitches'
-
+class Post(db.Model):
+    __tablename__ = "posts"
     id = db.Column(db.Integer,primary_key = True)
-    pitch_content = db.Column(db.String())
-    pitch_category = db.Column(db.String(255))
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post = db.Column(db.String)
+    time = db.Column(db.String)
+    comments = db.relationship("Comment",backref = "post", lazy = "dynamic")
 
-    def save_pitch(self):
+    def save_post(self):
         db.session.add(self)
         db.session.commit()
 
     @classmethod
     def get_pitch(cls,id):
-        pitches = Pitch.query.filter_by(id=id).all()
-        return pitches
+        posts = Post.query.filter_by(id=id).all()
+        return posts
 
     @classmethod
     def get_all_pitches(cls):
-        pitches = Pitch.query.order_by('-id').all()
-        return pitches
+        posts = Post.query.order_by('-id').all()
+        return posts
 
-    @classmethod
-    def get_category(cls,cat):
-        category = Pitch.query.filter_by(pitch_category=cat).order_by('-id').all()
-        return category
-
+    def get_post_comments(self):
+        comments= Comment.query.filter_by(post_id = self.id)
+        return comments
 
 class Comment(db.Model):
-    __tablename__='comments'
-
-    id = db.Column(db.Integer,primary_key=True)
-    comment_content = db.Column(db.String())
-    pitch_id = db.Column(db.Integer)
-    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    time = db.Column(db.String)
 
     def save_comment(self):
         db.session.add(self)
         db.session.commit()
 
-    @classmethod
-    def get_comments(cls,id):
-        comments = Comment.query.filter_by(pitch_id=id).all()
-        return comments
 
 class UpVote(db.Model):
     __tablename__ = 'upvotes'
 
     id = db.Column(db.Integer,primary_key=True)
     id_user = db.Column(db.Integer,db.ForeignKey('users.id'))
-    pitching_id = db.Column(db.Integer)
+    posting_id = db.Column(db.Integer)
 
     def save_vote(self):
         db.session.add(self)
@@ -109,18 +97,19 @@ class UpVote(db.Model):
 
     @classmethod
     def get_votes(cls,id):
-        upvote = UpVote.query.filter_by(pitching_id=id).all()
+        upvote = UpVote.query.filter_by(posting_id=id).all()
         return upvote
 
     def __repr__(self):
-        return f'{self.id_user}:{self.pitching_id}'
+        return f'{self.id_user}:{self.posting_id}'
+
 
 class DownVote(db.Model):
     __tablename__ = 'downvotes'
 
     id = db.Column(db.Integer,primary_key=True)
     id_user = db.Column(db.Integer,db.ForeignKey('users.id'))
-    pitching_id = db.Column(db.Integer)
+    posting_id = db.Column(db.Integer)
 
     def save_vote(self):
         db.session.add(self)
@@ -128,11 +117,11 @@ class DownVote(db.Model):
 
     @classmethod
     def get_downvotes(cls,id):
-        downvote = DownVote.query.filter_by(pitching_id=id).all()
+        downvote = DownVote.query.filter_by(posting_id=id).all()
         return downvote
 
     def __repr__(self):
-        return f'{self.id_user}:{self.pitching_id}'
+        return f'{self.id_user}:{self.posting_id}'
 
 class PhotoProfile(db.Model):
     __tablename__ = 'profile_photos'
