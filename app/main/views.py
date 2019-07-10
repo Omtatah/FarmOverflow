d27e5db0eacff5640b9c5bdfc646ff855b540b52
Antony <amwaniki180@gmail.com>
2019-07-10 12:14:52 +0300

    add main route index

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from . import main
 from flask_login import current_user, login_required
-from .forms import AddPostForm,SubscribeForm,AddComment,EditBio
-from ..models import Post,User,Comment
+# from .forms import AddPostForm,SubscribeForm,AddComment,EditBio
+from ..models import Post,User,Comment,UpVote,DownVote, PhotoProfile
 from flask import redirect,url_for,render_template,flash,request
 # from .. import db,photos
 from datetime import datetime
@@ -9,17 +9,8 @@ from app.email import create_mail
 
 @main.route("/", methods = ["GET","POST"])
 def index():
-    form = SubscribeForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        new_subscriber = Subscriber(email = email)
-        db.session.add(new_subscriber)
-        db.session.commit()
-        flash("Thank You for subscribing!")
-        return redirect(url_for("main.index"))
-    # posts = Post.query.order_by(Post.time.desc())
     title = "Home"
-    return render_template("index.html",form = form,title = title)
+    return render_template("index.html",title = title)
 
 @main.route("/add/post/",methods = ["GET","POST"])
 @login_required
