c46d7c59cb12c8ae0818260a0a3dc30aa73b2551
Antony <amwaniki180@gmail.com>
2019-07-10 12:06:44 +0300

    add user_id in class comment

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from . import main
 from flask_login import current_user, login_required
 from .forms import AddPostForm,SubscribeForm,AddComment,EditBio
-from ..models import Post,User,Comment,Subscriber
+from ..models import Post,User,Comment
 from flask import redirect,url_for,render_template,flash,request
 # from .. import db,photos
 from datetime import datetime
@@ -17,9 +17,9 @@ def index():
         db.session.commit()
         flash("Thank You for subscribing!")
         return redirect(url_for("main.index"))
-    posts = Post.query.order_by(Post.time.desc())
+    # posts = Post.query.order_by(Post.time.desc())
     title = "Home"
-    return render_template("index.html",posts = posts,form = form,title = title)
+    return render_template("index.html",form = form,title = title)
 
 @main.route("/add/post/",methods = ["GET","POST"])
 @login_required
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from app import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from time import time
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -17,7 +18,7 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String)
     bio = db.Column(db.String)
     image = db.Column(db.String)
-    post = db.relationship("Post", backref = "user", lazy = "dynamic")
+    posts = db.relationship("Post", backref = "user", lazy = "dynamic")
     user_pass = db.Column(db.String)
 
     comments = db.relationship('Comment',backref='user',lazy='dynamic')
@@ -56,14 +57,14 @@ class Post(db.Model):
         db.session.commit()
 
     @classmethod
-   def get_post(cls,id):
-       posts = Post.query.filter_by(id=id).all()
-       return posts
+    def get_post(cls,id):
+        posts = Post.query.filter_by(id=id).all()
+        return posts
 
-   @classmethod
-   def get_all_posts(cls):
-       posts = Post.query.order_by('-id').all()
-       return posts
+    @classmethod
+    def get_all_posts(cls):
+        posts = Post.query.order_by('-id').all()
+        return posts
 
     def get_post_comments(self):
         comments= Comment.query.filter_by(post_id = self.id)
@@ -73,9 +74,9 @@ class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
-    title = db.Column(db.String)
     content = db.Column(db.String)
     post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     time = db.Column(db.String)
 
     def save_comment(self):
